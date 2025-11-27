8d607841c65e51cce9c6114090443abd3d4d7ffd
aqtileksabitova-wq <aqtileksabitova@gmail.com>
2025-11-27 23:13:20 +0500

    Update client.ts

--- frontend/src/api/client.ts
+++ frontend/src/api/client.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import useAuthStore from "../store/auth";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL ?? "http://localhost:8000",
+  baseURL: import.meta.env.VITE_API_URL ?? "http://localhost:8000","https://suzie-undefalcated-carson.ngrok-free.dev " ,
   withCredentials: false,
 });
 
